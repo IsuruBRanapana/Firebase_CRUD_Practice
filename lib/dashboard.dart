9171f87070bc71376554a1fbb06db1468577e8e6
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-11-10 15:22:16 +0530

    Updated

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -11,6 +11,85 @@ class DashboardPage extends StatefulWidget{
 class _DashboardPageState extends State<DashboardPage>{
   String carModel;
   String carColor;
+  Future<bool> addDialog(BuildContext context) async{
+    return showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context){
+        return AlertDialog(
+          title: Text(
+            'Add Data',
+            style: TextStyle(
+              fontSize: 15.0
+            )
+          ),
+          content: Column(
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Enter Car Name"
+                ),
+                onChanged: (value){
+                  this.carModel=value;
+                },
+              ),
+              SizedBox(height: 5.0,),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Enter Car Color"
+                ),
+                onChanged: (value){
+                  this.carColor=value;
+                },
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text(
+                'Add'
+              ),
+              textColor: Colors.blue,
+              onPressed: (){
+                Navigator.of(context).pop();
+                Map carData={
+                  'carName':this.carModel,
+                  'color':this.carColor
+                  };
+                crudObj.addData(carData).then((result){
+                  dialogTrigger(context);
+                }).catchError((e){
+                  print(e);
+                });
+              },
+            )
+          ],
+        );
+      }
+    );
+  }
+
+  Future<bool> dialogTrigger(BuildContext context)async{
+    return showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context){
+        return AlertDialog(
+          title: Text('Job Done',
+          style: TextStyle(fontSize: 15.0)),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('All Right'),
+              textColor: Colors.blue,
+              onPressed: (){
+                Navigator.of(context).pop();
+              },
+            )
+          ]
+        )
+      }
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
